a9ef15d694815528e52c8bffa889258be59e0aaa
Eveerme <yx3109970873@qq.com>
2022-11-23 14:01:41 +0800

    Vue Version-1.1 -----登录页面完善

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,9 +6,9 @@
     </div>
     <el-dropdown style="width: 100px; cursor: pointer">
       <div style="display: inline-block">
-        <img :src="user.avatarUrl" alt=""
+        <img :src="admin.avatar" alt=""
              style="width: 30px; border-radius: 50%; position: relative; top: 10px; right: 5px">
-        <span>{{ user.nickname }}</span>
+        <span>{{ admin.adminNickname }}</span>
         <i class="el-icon-arrow-down" style="margin-left: 5px"></i>
       </div>
       <el-dropdown-menu slot="dropdown">
@@ -36,13 +36,13 @@ export default {
   },
   data(){
     return {
-      user: localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")) : {}
+      admin: localStorage.getItem("admin") ? JSON.parse(localStorage.getItem("admin")) : {}
     }
   },
   methods: {
     logout(){
       this.$router.push("/login")
-      localStorage.removeItem("user")
+      localStorage.removeItem("admin")
       this.$message.success("退出成功")
     }
   }
--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -2,12 +2,12 @@
   <div class="wrapper">
     <div class="login-box">
       <h2 class="h2-wh">登录系统</h2>
-      <el-form :rules="rules" :model="user" ref="userForm">
-        <el-form-item prop="username">
-          <el-input size="medium" class="m-10" prefix-icon="el-icon-user" v-model="user.username"></el-input>
+      <el-form :rules="rules" :model="admin" ref="adminForm">
+        <el-form-item prop="adminCount">
+          <el-input size="medium" class="m-10" prefix-icon="el-icon-user" v-model="admin.adminCount"></el-input>
         </el-form-item>
         <el-form-item prop="password">
-          <el-input size="medium" class="m-10" prefix-icon="el-icon-lock" show-password v-model="user.password"></el-input>
+          <el-input size="medium" class="m-10" prefix-icon="el-icon-lock" show-password v-model="admin.password"></el-input>
         </el-form-item>
         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
         <el-button type="primary" @click="handleSubmit">
@@ -30,12 +30,12 @@ export default {
   name: "Login",
   data() {
     return {
-      user: {
-        username: "admin",
-        password: "admind"
+      admin: {
+        adminCount: "admin",
+        password: "admin"
       },
       rules: {
-        username: [
+        adminCount: [
           { required: true, message: '请输入用户名', trigger: 'blur' },
           { min: 3, max: 10, message: '长度在 3 到 5 个字符', trigger: 'blur' }
         ],
@@ -48,13 +48,13 @@ export default {
   },
   methods: {
     handleSubmit(){
-      this.$refs['userForm'].validate((valid) => {
+      this.$refs['adminForm'].validate((valid) => {
         if (valid) {
           //表单校验合法
-          this.request.post("/user/login",this.user).then(res => {
+          this.request.post("/admin/login",this.admin).then(res => {
             console.log(res)
             if (res.status=="200"){
-              localStorage.setItem("user",JSON.stringify(res.data)) //  存储用户信息到浏览器
+              localStorage.setItem("admin",JSON.stringify(res.data)) //  存储用户信息到浏览器
               this.$router.push("/")
               this.$message.success(res.msg)
             }else {
